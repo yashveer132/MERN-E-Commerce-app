68c957b6de8f061a8cc8ae49f335709600378247
Yash Veer Singh <ys3792@srmist.edu.in>
2023-04-18 00:52:49 +0530

    Updated Server.js

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from 'url';
 
 dotenv.config();
 
 connectDB();
+const __filename= fileURLToPath(import.meta.url);
+const __dirname= path.dirname(__filename);
 
 const app = express();
 
